6b748fc1d760d97d71bd1f1416720c0f9915c449
stefanappdev <stevendking@protonmail.com>
2024-11-09 14:43:16 -0800

    works on md to sm screens

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-
-
-
 let bg=document.querySelector("#app-bg");
-let themeslider=document.querySelector("#theme-settings-img");
+let themesettings=document.querySelector("#theme-settings");
+
 
 
 //to change theme from light to dark
 
+themesettings.addEventListener("click",toogleTheme);
 
-if(themeslider.classList.contains("light")){
+
+const toogleTheme=()=>{
+	if(themesettings.classList.contains("light")){
 	
-	themeslider.classList.remove("light");
-	themeslider.classlist.add("dark");
-	bg.setAttribute("backgroundColor","#3443eb");
-	themeslider.setAttribute("src","./images/toogle-on");
+		themesettings.classList.add("dark");
+		themesettings.classList.remove("light");
+		
+		bg.style.backgroundColor="#211c41";
+		bg.style.color="white";
+		themesettings.setAttribute("src","./images/toggle-on.png");
+		
+		
+	}else if(themesettings.classList.contains("dark")){
+		themesettings.classList.add("light");
+		themesettings.classList.remove("dark");
+		bg.style.color="black";
+		bg.style.backgroundColor="#efeff6";
+		themesettings.setAttribute("src","./images/toggle-off.png");
+	}
 	
-	
-}else if(themeslider.classList.contains("dark")){
-	themeslider.classList.remove("dark");
-	themeslider.classlist.add("light");
-	bg.setAttribute("backgroundColor","#efeff6;");
-	themeslider.setAttribute("src","./images/toogle-off");
+	console.log("sucess");
 }
+
+
+
